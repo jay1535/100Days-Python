6ff4170608a673b6a1edcd277d383dfc7ba65e47
jay1535 <habbujayanth@gmail.com>
2024-11-10 14:22:10 +0530

    Added Scoreboard and Gameover

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 
@@ -12,6 +13,7 @@ screen.tracer(0)
 
 snake= Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up,"Up")
@@ -31,6 +33,18 @@ while game_is_on:
 
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+    
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor()>280 or snake.head.ycor()< -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment)<10:
+            game_is_on = False
+            scoreboard.game_over()
+
 
 
 
