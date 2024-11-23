b25292d9f3778a62816d9d3fe35d85c4f8fb30a1
jay1535 <habbujayanth@gmail.com>
2024-11-23 18:08:17 +0530

    Missing States

--- US-State/Guess/main.py
+++ US-State/Guess/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S States Game")
@@ -6,8 +7,37 @@ image = "100Days-Python/US-State/Guess/blank_states_img.gif"
 
 screen.addshape(image)
 turtle.shape(image)
+data = pandas.read_csv("100Days-Python/US-State/Guess/50_states.csv")
+all_states=data.state.to_list()
+guessed_states =[]
 
-answer_state = screen.textinput(title="GUESS THE STATE", prompt="What's another state")
+
+while len(guessed_states) <50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 states correct", prompt="What's another state").title()
+    print(answer_state)
 
 
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("100Days-Python/US-State/Guess/missing_states.csv")
+        break
+
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item() , state_data.y.item())
+        t.write(answer_state)
+
+    
+
+
+screen.exitonclick()
 
