e3fb05aea1e685e45afe40e460a331eb6772278b
jay1535 <habbujayanth@gmail.com>
2024-12-23 18:47:52 +0530

    JOB

--- COOKIE/main.py
+++ COOKIE/main.py
@@ -1,5 +1,5 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium import webdriver # type: ignore
+from selenium.webdriver.common.by import By # type: ignore
 import time
 
 
